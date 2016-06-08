06019decd19a3d8912b40aaf8b88476c5c371425
menato <shmakir@yandex.ru>
2016-06-08 17:24:08 +0300

    Preliminary parsing for csu added.

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -4,52 +4,79 @@ import scala.io.Source
 import scalaj.http._
 
 object Parser {
-  def downloadChapters(baseDir: String, chapters: Int, url: StringContext): Unit = {
+  def downloadChapters(baseDir: String, firstId: Int, lastId: Int, url: StringContext): Unit = {
     new File(baseDir) mkdir()
-    for (chapterId <- 1 to chapters) {
+    for (chapterId <- firstId to lastId) {
       val response: HttpResponse[String] = Http(url.s(chapterId)).asString
       val writer = new PrintWriter(baseDir + s"/$chapterId.html")
       writer.print(response.body)
       writer.close()
       Thread sleep 1000
     }
+  }
 
+  //    val pattern = """<span class=snosCit>[^<]+</span><font color=green>\*</font>""".r
+  //
+  //    for(line <- Source.fromFile("inp.txt").getLines) {
+  //      val matches = pattern.findAllIn(line)
+  //      for (m <- matches)
+  //        println(m)
+  //    }
+
+
+  def parseRussian(lines: Iterator[String], chapterId: Int, writer: PrintWriter): Unit = {
+    var flag = false
+    val prefix = "<li class=\"verse visible-1\"><div class=\"verse-inner\">"
+    val suffix = "</div></li>"
+    var verseId = 0
+
+    lines.foreach(line => {
+      flag = flag || (line contains "<ul id=\"list-verses-r\" class=\"verses\">")
+      flag = flag && !(line contains "</ul>")
+      if (flag && (line contains prefix) && (line contains suffix)) {
+        val beg = (line indexOf prefix) + prefix.length
+        val end = line indexOf suffix
+        verseId += 1
+        writer.printf("    (\"ru\", \"Mt\", %d, %d, \"%s\"),\n",
+          int2Integer(chapterId), int2Integer(verseId), line.substring(beg, end))
+      }
+    })
+  }
+
+  def parseSlavonic(lines: Iterator[String], chapterId: Int, writer: PrintWriter): Unit = {
+    var flag = false
+    val prefix = "<li class=\"verse visible-1\"><div class=\"verse-inner\">"
+    val suffix = "</div></li>"
+    var verseId = 0
+
+
+    lines.foreach(line => {
+      flag = flag || (line contains "<ul id=\"list-verses-ucs\" class=\"verses sublang lang-ucs\" >")
+      flag = flag && !(line contains "</ul>")
+      if (flag && (line contains prefix) && (line contains suffix)) {
+        val beg = (line indexOf prefix) + prefix.length
+        val end = line indexOf suffix
+        val verse = line.substring(beg, end).replace(";", ";;").replaceAll("""<[^>]+>""", "")
+        verseId += 1
+        writer.printf("    (\"csu\", \"Mt\", %d, %d, \"%s\"),\n",
+          int2Integer(chapterId), int2Integer(verseId), verse)
+      }
+    })
+
+    writer.println();
   }
 
   def main(args: Array[String]): Unit = {
-//    val pattern = """<span class=snosCit>[^<]+</span><font color=green>\*</font>""".r
-//
-//    for(line <- Source.fromFile("inp.txt").getLines) {
-//      val matches = pattern.findAllIn(line)
-//      for (m <- matches)
-//        println(m)
-//    }
+//    downloadChapters("Mt", 25, 28, StringContext("http://azbyka.ru/biblia/?Mt.", "&ucs"))
+//    downloadChapters("Lc", 1, 24, StringContext("http://azbyka.ru/biblia/?Lk.", "&ucs"))
 
-//    downloadChapters("Mt", 28, StringContext("http://azbyka.ru/biblia/?Mt.", "&ucs"))
-//    downloadChapters("Lc", 24, StringContext("http://azbyka.ru/biblia/?Lk.", "&ucs"))
+    val writer = new PrintWriter("some.sql")
 
+    for (chapterId <- 1 to 28) {
+      val lines = Source.fromFile(s"Mt/$chapterId.html").getLines
+      parseSlavonic(lines, chapterId, writer)
+    }
 
-//    for (chapterId <- 1 to 28) {
-
-//      val lines = response.body.split("\n")
-//
-//
-//      var flag = false
-//      val prefix = "<li class=\"verse visible-1\"><div class=\"verse-inner\">"
-//      val suffix = "</div></li>"
-//      var verseId = 0
-//
-//      lines.foreach(line => {
-//        flag = flag || (line contains "<ul id=\"list-verses-r\" class=\"verses\">")
-//        flag = flag && !(line contains "</ul>")
-//        if (flag && (line contains prefix) && (line contains suffix)) {
-//          val beg = (line indexOf prefix) + prefix.length
-//          val end = line indexOf suffix
-//          verseId += 1
-//          writer.printf("    (\"ru\", \"Mt\", %d, %d, \"%s\"),\n",
-//            int2Integer(chapterId), int2Integer(verseId), line.substring(beg, end))
-//        }
-//      })
-//    }
+    writer.close()
   }
 }
